077403347beea15c92dd7012d2234d08022dd100
johanxr1 <johan.tenias@gmail.com>
2023-12-03 22:00:09 -0400

    Conetion and nodemon

--- Connection/conexion.js
+++ Connection/conexion.js
@@ -1,11 +1,16 @@
 import mysql from "mysql2";
 
+const HOST = process.env.HOST || "localhost";
+const USERDB = process.env.USERDB || "root";
+const PASSWORD = process.env.PASSWORD || "";
+const DATABASE = process.env.DATABASE || "alimentosdb";
+
 // Create the connection pool. The pool-specific settings are the defaults
 export const pool = mysql.createPool({
-  host: "localhost",
-  user: "root", //nutrqwpv_alimentosuser ESTE usuario solo puede hacer SELECT
-  database: "alimentosdb", // nutrqwpv_alimentosdb
-  password: "", //.r^}L-jHp#h5
+  host: HOST,
+  user: USERDB, //nutrqwpv_alimentosuser ESTE usuario solo puede hacer SELECT
+  database: DATABASE, // nutrqwpv_alimentosdb
+  password: PASSWORD, //.r^}L-jHp#h5
   waitForConnections: true,
   connectionLimit: 10,
   maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
